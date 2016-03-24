c27919e0902f7355cacba9b127dfc0f0cfebb71d
podviaznikov <podviaznikov@gmail.com>
2016-03-23 17:30:49 -0700

    update test

---
+++ client.js
@@ -0,0 +1,10 @@
+var http = require("http");
+var Primus = require("primus");
+
+var primus = Primus.connect("http://test-ws-server-staging-runnable.runnableapp.com:80")
+primus.on("open", function() {
+  console.log("Connected!")
+})
+primus.on("data", function(data) {
+  console.log('data', data, new Date().getTime())
+})
