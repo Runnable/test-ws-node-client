917df179f3d4fbe9d0985aaa39143060e2a592ff
Anton Podviaznikov <podviaznikov@gmail.com>
2016-04-14 12:11:24 -0700

    Update client.js

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var Primus = require("primus");
 
 var primus = Primus.connect("http://test-ws-server-staging-runnable.runnableapp.com:80")
 primus.on("open", function() {
-  console.log("Connected!!")
+  console.log("Connected?!!")
 })
 primus.on("data", function(data) {
   console.log('data', data, new Date().getTime())
